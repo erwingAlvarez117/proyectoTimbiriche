3ad281ba79dfd2dfd4e348e642db803f1f85948d
erwingAlvarez117 <erwing630@gmail.com>
2021-05-22 14:59:40 -0700

    Actualización del proyecto

---
+++ Datos/src/datos/IpServidor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+/**  
+ *
+ * @author pc
+ */
+public class IpServidor {
+   
+    public static String ipServidor = "localhost";
+    
+    
+    public static int puertoServidor = 2027;
+}
